9e229b4dcd0574477b64ea14f08438a8eb291770
vani-id98 <vkonda673@gmail.com>
2023-11-24 15:04:54 +0530

    message

---
+++ src/main/java/com/kmit/maven_pjt/App.java
@@ -0,0 +1,13 @@
+package com.kmit.maven_pjt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
